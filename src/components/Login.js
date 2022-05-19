d2324e5275c7248b77c32824ba4b480e2d0da662
KimMinJae <smallkdb@gmail.com>
2022-05-20 01:48:07 +0900

    finish

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../redux/user";
 
 function Login() {
+	const dispatch = useDispatch();
 	return (
 		<div>
-			<button>Login</button>
+			<button
+				onClick={() => {
+					dispatch(
+						login({ name: "minjaekim", age: 23, email: "skynickmj@naver.com" })
+					);
+				}}
+			>
+				Login
+			</button>
+			<button
+				onClick={() => {
+					dispatch(logout());
+				}}
+			>
+				Logout
+			</button>
 		</div>
 	);
 }
